2039e724fff2f77e38ca57242cb5ffe6d0930429
SnakeMind <snakemind@gmail.com>
2018-09-30 20:32:33 +0200

    After-Install done. Next scripts will deal with customisations

--- after-install.sh
+++ after-install.sh
@@ -22,7 +22,7 @@ STAGE=0
 TOTAL=$( grep '(${STAGE}/${TOTAL})' $0 | wc -l );(( TOTAL-- ))
 
 ## Are we root?
-if [[ "${EUID}" -ne 0]]; then
+if [[ "${EUID}" -ne 0 ]]; then
   echo -e ' '${RED}'[!]'${RESET}" Run this as ${RED}root${RESET}. Quitting now..." 1>&2
   exit 1
 else
@@ -79,11 +79,11 @@ cat <<EOF > "${file}" \
 
 echo -e "** Mounting Stack **"
 bash /usr/local/sbin/mount-shared-folders
-echo -e "** PostgreSQL **"
+echo -e "\n** PostgreSQL **"
+echo -e '[i] (Re)starting PostgreSQL Service \n'
 systemctl restart postgresql
 sleep 1s
-echo ' '
-echo "** Okay... We're done! **\n"
+echo -e "** Okay... We're done! **\n"
 
 EOF
 chmod +x "${file}"
@@ -119,3 +119,94 @@ sed -i '/kali/ s/^\( \|\t\|\)deb cdrom/#deb cdrom/g' "${file}"
 
 ##- incase we were interrupted
 dpkg --configure -a
+
+## Update
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL})  Updating APT information"
+apt -qq update
+if [[ "$?" -ne 0 ]]; then
+  echo -e ' '${RED}'[!]'${RESET}" There is an issue getting updates${RESET}" 1>&2
+  echo -e " ${YELLOW}[i]${RESET} Check your local network repository information:\n" 
+  curl -sI http://http.kali.org/README
+  exit 1
+fi
+
+sleep 5s
+## Install VMware tools
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing open-vm-tools and shizzle."
+apt -qq install open-vm-tools open-vm-tools-desktop fuse make -y
+
+sleep 5s
+## Setting Time
+(( STAGE++ )); echo -e " ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ntpdate"
+apt -y -qq install ntp ntpdate
+ntpdate -b -s -u pool.ntp.org
+systemctl restart ntp
+systemctl enable ntp 2>/dev/null
+start_time=$(date +%s)
+
+sleep 5s
+## Full update
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Updating OS"
+for FILE in clean autoremove; do apt -y -qq "${FILE}"; done
+export DEBIAN_FRONTEND=noninteractive
+apt -qq update && APT_LISTCHANGES_FRONTEND=none apt -o Dpkg::Options::="--force-confnew" -y dist-upgrade --fix-missing 2>&1
+
+for FILE in clean autoremove; do apt -y -qq "${FILE}"; done
+fi
+
+sleep 5s
+## Install kernel headers
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}kernel headers${RESET}"
+apt -y -qq install gcc "linux-headers-$(uname -r)"
+if [[ $? -ne 0 ]]; then
+  echo -e ' '${RED}'[!]'" There was an issue installing kernel headers${RESET}" 1>&2
+  exit 1
+fi
+
+sleep 5s
+## Install "kali full" meta packages (default tool selection)
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing kali-linux-full meta-package"
+apt -y -qq install kali-linux-full
+
+sleep 5s
+## Configure GRUB
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Configuring GRUB boot manager"
+grubTimeout=5
+(dmidecode | grep -iq virtual) && grubTimeout=1
+file=/etc/default/grub; [ -e "${file}" ] && cp -n $file{,.bkup}
+sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT='${grubTimeout}'/' "${file}"
+sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="vga=0x0318"/' "${file}"
+update-grub
+
+sleep 5s
+## Clean the system
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Cleaning the system"
+
+##- Clean package manager
+for FILE in clean autoremove; do apt -y -qq "${FILE}"; done
+apt -y -qq purge $(dpkg -l | tail -n +6 | egrep -v '^(h|i)i' | awk '{print $2}')
+
+##- Update slocate database
+updatedb
+
+##- Reset folder location
+cd ~/ &>/dev/null
+
+##- Remove any history files (as they could contain sensitive info)
+history -cw 2>/dev/null
+for i in $(cut -d: -f6 /etc/passwd | sort -u); do
+  [ -e "${i}" ] && find "${i}" -type f -name '.*_history' -delete
+done
+
+## Time taken
+finish_time=$(date +%s)
+echo -e "\n\n ${YELLOW}[i]${RESET} Time (roughly) taken: ${YELLOW}$(( $(( finish_time - start_time )) / 60 )) minutes${RESET}"
+
+## Done!
+echo -e "\n ${YELLOW}[!]${RESET} So. We're done. Please:
+echo -e " ${YELLOW}[i]${RESET} + Setup git:   ${YELLOW}git config --global user.name <name>;git config --global user.email <email>${RESET}"
+echo -e " ${YELLOW}[i]${RESET} + ${YELLOW}Reboot${RESET}"
+echo -e " ${YELLOW}[i]${RESET} + Take a snapshot"
+
+echo -e '\n'${BLUE}'[*]'${RESET}' '${BOLD}'Done!'${RESET}'\n\a'
+exit 0
